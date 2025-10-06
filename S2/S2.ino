c729d0cdb1d4d39b62ff37c90fff93a47d7c093e
Enzo <enzo_caetano@estudante.sesisenai.org.br>
2025-10-06 12:46:52 -0300

    UPDATE: S2 com reconexão automática

--- S2/S2.ino
+++ S2/S2.ino
@@ -7,17 +7,31 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Conectando ao WiFi");
   WiFi.begin(SSID,PASS);
+
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
   }
 
-   Serial.println("\nConectado!");
-   Serial.println(WiFi.localIP());
+  Serial.println("\nConectado!");
+  Serial.println("IP:");
+  Serial.print(WiFi.localIP());
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (WiFi.status() != WL_CONNECTED){
 
+    Serial.println("Conectando ao WiFi");
+    WiFi.begin(SSID,PASS);
+
+    while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(200);
+    }
+
+    Serial.println("\nConectado!");
+    Serial.println("IP:");
+    Serial.print(WiFi.localIP());
+  }
 }
