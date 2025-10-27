844270f4f34643ed165f67304d387298e331f274
Enzo <enzo_caetano@estudante.sesisenai.org.br>
2025-10-27 12:41:18 -0300

    envio e recebimento de msgs

--- S2/S2.ino
+++ S2/S2.ino
@@ -9,9 +9,14 @@ const String PASS = "8120gv08";
 
 const int PORT           = 1883;
 const String URL         = "test.mosquitto.org";
-const String Topic       = "DSM2";
+
+const String MyTopic = "TopicoChat";
+const String OtherTopic = "TopicoChat";
+
 const String broker_user = "";
 const String broker_pass = "";
+
+const int LED = 2;
 
 void setup() {
   Serial.begin(115200);
@@ -27,18 +32,23 @@ void setup() {
   Serial.println("IP:");
   Serial.print(WiFi.localIP());
 
-  Serial.println("Conectando ao broker");
+  Serial.println("\nConectando ao broker");
   mqtt.setServer(URL.c_str(), PORT);
 
   while(!mqtt.connected()){
     String ID = "S2-";
     ID += String(random(0xffff), HEX);
+
     mqtt.connect(ID.c_str(), broker_user.c_str(), broker_pass.c_str());
-    Serial.print(".");
     delay(200);
+    Serial.print(".");
   }
 
+  mqtt.subscribe(MyTopic.c_str());
+  mqtt.setCallback(callback);
   Serial.println("\nConectado ao broker com sucesso!");
+
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
@@ -56,11 +66,30 @@ void loop() {
     Serial.println("IP:");
     Serial.print(WiFi.localIP());
 
-    String mensagem = "Enzo: ";
-    mensagem += "oi";
+  }
 
-    mqtt.publish(Topic.c_str(), mensagem.c_str());
+    String mensagem ="Enzo: ";
+
+    if(Serial.available()>0){
+      mensagem += Serial.readStringUntil('\n');
+      mqtt.publish(OtherTopic.c_str(),mensagem.c_str());
+  }
     mqtt.loop();
-    delay(200);
+    delay(500);
+}
+void callback(char* topic, byte* payload, unsigned int length){
+  String mensagem = "";
+  for(int i = 0; i < length; i++){
+    mensagem += (char)payload[i];
+  }
+  Serial.print("Recebido: ");
+  Serial.println(mensagem);
+
+  if(mensagem == "Marco: AcendeR" || mensagem == "Carlos: AcendeR" || mensagem == "Brayan: AcendeR" || mensagem == "Enzo: AcendeR"){
+    digitalWrite(LED, HIGH);
+  }else if(mensagem == "Marco: ApagaR" || mensagem == "Carlos: ApagaR" || mensagem == "Brayan: ApagaR" || mensagem == "Enzo: ApagaR"){
+    digitalWrite(LED, LOW);
+  }else{
+    Serial.println(mensagem);
   }
 }
