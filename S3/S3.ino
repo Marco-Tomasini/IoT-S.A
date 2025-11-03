256b30bd1a1dbc82b929f04e9c70d87460b7665e
Marco-Tomasini <marco_tomasini@estudante.sesisenai.org.br>
2025-11-03 12:53:28 -0300

    Iniciando comunicação e topicos do servo1 e presença1

--- S3/S3.ino
+++ S3/S3.ino
@@ -1,27 +1,33 @@
-#include <WiFi.h>
+#include <WiFiClientSecure.h>
 #include <PubSubClient.h>
+#include <ESP32Servo.h>
 
-WiFiClient client;            //cria o cliente wifi
+WiFiClientSecure client;            //cria o cliente wifi
 PubSubClient mqtt(client);    //fala que o mqtt usa o cliene wifi
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
 
-const int PORT = 1883;
-const String URL = "test.mosquitto.org";
+const int PORT = 8883;
+const String URL = "81e7fafe091e4b09b0b93bf45fb52950.s1.eu.hivemq.cloud";
 
-const String broker_user = "";
-const String broker_pass = "";
+const String broker_user = "s3_Marco";
+const String broker_pass = "Loscrias#67";
 
-const String MyTopic = "TopicoChat"; //define de onde vou receber as mensagem
-const String OtherTopic = "TopicoChat"; //define para onde vou enviar as mensagens
+const String s1 = "Servo1"; //define de onde vou receber as mensagem
+const String p1 = "Presenca1"; //define para onde vou enviar as mensagens
 
-const int LED = 2;
+const int servoPin1 = 16;
+Servo servo1;
+const int estado_00 = 120;
+const int estado_01 = 60;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Conectando ao WiFi");
   WiFi.begin(SSID,PASS);
+
+  servo1.attach(servoPin1);
 
   pinMode(LED,OUTPUT);
 
@@ -34,6 +40,8 @@ void setup() {
   Serial.println("IP:");
   Serial.print(WiFi.localIP());
 
+  client.setInsecure();
+
   Serial.print("Conectando ao broker...");
   mqtt.setServer(URL.c_str(),PORT);
 
@@ -45,7 +53,7 @@ void setup() {
     delay(200);
     Serial.print(".");
   }
-  mqtt.subscribe(MyTopic.c_str());
+  mqtt.subscribe(s1.c_str());
   mqtt.setCallback(callback);
   Serial.println("\n Conecado ao broker com sucesso!");
 
@@ -67,12 +75,6 @@ void loop() {
     Serial.print(WiFi.localIP());
   }
 
-  String mensagem = "Marco: ";
-
-  if(Serial.available()>0){
-    mensagem += Serial.readStringUntil('\n'); //lê mensagem digitada
-    mqtt.publish(OtherTopic.c_str(),mensagem.c_str()); //envia a mensagem para o tópico
-  }
   mqtt.loop();
   delay(500);
 }
@@ -85,13 +87,17 @@ void callback(char* topic, byte* payload, unsigned int length){
   Serial.print("Recebido: ");
   Serial.println(mensagem);
 
-  if(mensagem == "Carlos: acender" || mensagem == "Enzo: acender" || mensagem == "Brayan: acender" || mensagem == "Vitor: acender" || mensagem == "Marco: acender"){
-    digitalWrite(LED, HIGH);
-    Serial.println("Led Ligado");
-  }
+  if(topic == p1){
+    if(mensagem == "Estado_0"){
+      servo1.write(estado_00); 
+      Serial.println("Estado 00 Ativado");
+      mqtt.publish(s1.c_str(),"Estado_0");
+    }
 
-  if(mensagem == "Carlos: apagar" || mensagem == "Enzo: apagar" || mensagem == "Brayan: apagar" || mensagem == "Vitor: apagar" || mensagem == "Marco: apagar"){
-    digitalWrite(LED, LOW);
-    Serial.println("Led Desligado");
+    if(mensagem == "Estado_1"){
+      meuservo.write(estado_01); 
+      Serial.println("Estado 01 Ativado");
+      mqtt.publish(s1.c_str(),"Estado_1");
+    }
   }
 }
